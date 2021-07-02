3f4701aa4b0cb9979d3b3434624496d34df18b56
evgeny <evgenykalentyew@mail.ru>
2021-07-02 16:49:23 +0300

    first practice

--- project1/js/script.js
+++ project1/js/script.js
@@ -1,1 +1,16 @@
-console.log(1);
+'use strict';
+let numberOfFilms =prompt('Сколько фильмов вы уже посмотрели?',100);
+let personalMovieDB =  
+    {count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false};
+let lastFilm1 = prompt('Последний просмотренный вами фильм?','');
+let reit1 = prompt('На сколько оцениваете его?','');
+let lastFilm2 = prompt('Последний просмотренный вами фильм?','');
+let reit2 = prompt('На сколько оцениваете его?','');
+personalMovieDB.movies[lastFilm1]=reit1;
+personalMovieDB.movies[lastFilm2]=reit2;
+
+console.log(personalMovieDB);
